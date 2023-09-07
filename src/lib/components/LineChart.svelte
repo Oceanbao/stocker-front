a9aff4fd6b3efb51ce7587740b1dfc30f97e0f1c
oceanbao <oceanbao@outlook.com>
2023-09-07 15:06:27 +0800

    INIT

---
+++ src/lib/components/LineChart.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { Line } from 'svelte-chartjs';
+	import { stock } from './data';
+
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		LineElement,
+		LinearScale,
+		PointElement,
+		CategoryScale,
+		Filler
+	} from 'chart.js';
+
+	ChartJS.register(
+		Title,
+		Tooltip,
+		Legend,
+		LineElement,
+		LinearScale,
+		PointElement,
+		CategoryScale,
+		Filler
+	);
+</script>
+
+<!-- <Line {data} options={{ responsive: true }} /> -->
+<Line data={stock} options={{ responsive: true }} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import LineChart from '$lib/components/LineChart.svelte';
+</script>
+
+<main class="chart-container">
+	<LineChart />
+</main>
+
+<style>
+	main {
+		position: relative;
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
