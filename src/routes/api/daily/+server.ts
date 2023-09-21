dc41e6b9fb14e9913f9b3092c34c3361a97ab70a
oceanbao <oceanbao@outlook.com>
2023-09-21 19:41:23 +0800

    update: new api for daily and finish chart

---
+++ src/routes/api/daily/+server.ts
@@ -0,0 +1,28 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async (event) => {
+	const code = event.url.searchParams.get('code');
+	if (!code) {
+		return new Response(
+			JSON.stringify({
+				message: 'require code'
+			})
+		);
+	}
+
+	const today = new Date();
+	const xDayAgo = 120;
+	const thatDate = new Date(today - 1000 * 60 * 60 * 24 * xDayAgo);
+	const thatDateFormatted = thatDate.toJSON().slice(0, 10);
+
+	const records = await event.locals.pb?.collection('daily').getFullList({
+		filter: `code = "${code}" && date >= "${thatDateFormatted} 00:00:00"`
+	});
+
+	return new Response(
+		JSON.stringify({
+			message: 'ok',
+			records
+		})
+	);
+};
--- src/routes/rsi/+page.svelte
+++ src/routes/rsi/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import ChartRsi from '$lib/components/ChartRSI.svelte';
+
+	import { generateCandlestickData } from '$lib/genData.js';
+
 	export let data;
 
 	let { records } = data;
@@ -6,10 +10,38 @@
 	records = records.map((x) => ({ code: x.code, rsi: x.rsi, name: x.name, cap: x.cap }));
 	// records.sort((a, b) => (a.rsi > b.rsi ? 1 : b.rsi > a.rsi ? -1 : 0));
 	records.sort((a, b) => (a.cap > b.cap ? -1 : b.cap > a.cap ? 1 : 0));
+
+	let candleData = generateCandlestickData();
+	let rsiData = calculateRSI(candleData, 6);
+
+	function calculateRSI(data, count) {
+		var rsi = function (data) {
+			var gain = 0;
+			var loss = 0;
+			for (var i = 1; i < data.length; i++) {
+				var diff = data[i].close - data[i - 1].close;
+				if (diff < 0) {
+					loss += Math.abs(diff);
+				} else {
+					gain += diff;
+				}
+			}
+			return 100 - 100 / (1 + gain / data.length / (loss / data.length));
+		};
+		var result = [];
+		for (var i = count; i < data.length; i++) {
+			var val = rsi(data.slice(i - count, i));
+			result.push({ time: data[i].time, value: val });
+		}
+		return result;
+	}
 </script>
 
-<main class="grid place-items-center my-12 mx-20">
+<main class="grid place-items-center">
 	<p>Total: {records.length}</p>
+	<div class="w-[80vw] h-[33vh] m-auto mb-16">
+		<ChartRsi {candleData} {rsiData} />
+	</div>
 	<div class="space-y-8 w-full">
 		{#each records as record}
 			<div class="flex items-center">
